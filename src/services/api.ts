3cdee5ccb3eeca90df7afd9bba3bd6976c0e0f06
judithodo <odojudith18@gmail.com>
2026-08-14 20:46:25 +0100

    setting up

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { ProductsResponse } from "../types/product";
+
+export const api = createApi({
+  reducerPath: "api",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://dummyjson.com",
+  }),
+
+  endpoints: (builder) => ({
+    getProducts: builder.query<ProductsResponse, void>({
+      query: () => "/products",
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = api;
